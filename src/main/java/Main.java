fe02c15830fc9f946fd58d69b6d9593266812e32
HP <tgoldsmith01@yahoo.com>
2021-08-29 20:33:19 -0400

    Newest Version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,18 +5,16 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String Str1="";
-        String Str2="";
         Scanner input= new Scanner(System.in);
         System.out.print("What is the first number? ");
-        Str1=input.nextLine();
+        String Str1=input.nextLine();
         System.out.print("What is the second number? ");
-        Str2=input.nextLine();
+        String Str2=input.nextLine();
         int a= Integer.parseInt(Str1);
         int b= Integer.parseInt(Str2);
-        System.out.println(Str1+" + "+Str2+" = "+(a+b));
-        System.out.println(Str1+" - "+Str2+" = "+(a-b));
-        System.out.println(Str1+" * "+Str2+" = "+(a*b));
-        System.out.println(Str1+" / "+Str2+" = "+(a/b));
+        System.out.println(Str1+" + "+Str2+" = "+(a+b)+"\n"+
+                Str1+" - "+Str2+" = "+(a-b)+"\n"+
+                Str1+" * "+Str2+" = "+(a*b)+"\n"+
+                Str1+" / "+Str2+" = "+(a/b));
     }
 }
